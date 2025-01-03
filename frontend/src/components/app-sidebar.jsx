ab90359252d81f47253eef72c91b2f710e4694cc
deepkiyada <deep.kiyada@marutitech.com>
2025-01-03 15:18:01 +0530

    sidebar and structure added

---
+++ frontend/src/components/app-sidebar.jsx
@@ -0,0 +1,67 @@
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+} from "./ui/sidebar.jsx";
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Transactions",
+    url: "/transactions",
+    icon: Inbox,
+  },
+  {
+    title: "Income",
+    url: "/",
+    icon: Calendar,
+  },
+  {
+    title: "Expense",
+    url: "/", 
+    icon: Search,
+  },
+  {
+    title: "Categories",
+    url: "/",
+    icon: Settings,
+  },
+];
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  );
+}
