e32a65e3c250d688dd23e6849b92b7b54771b36f
Kuldip271 <kuldip.karangiya@marutitech.com>
2025-01-03 16:54:25 +0530

    all changes

--- frontend/src/app/expenses/page.js
+++ frontend/src/app/expenses/page.js
@@ -1,7 +1,30 @@
-export default function Expenses() {
+"use client";
+import { useState } from "react";
+import FormComponent from "../../components/Form";
+import TableComponent from "../../components/AllTransaction";
+// import styles from "./TablePage.module.css";
+
+export default function TablePage() {
+  const [entries, setEntries] = useState([]);
+
+  const handleAddEntry = (entry) => {
+    setEntries((prevEntries) => [...prevEntries, entry]);
+  };
+
   return (
     <div>
-      <h1>Expenses</h1>
+      <h2>Expense Tracker</h2>
+      <div
+        style={{
+          display: "flex",
+          height: "100vh",
+          background: "#f2f2f2",
+          width: "83vw",
+        }}
+      >
+        <FormComponent onAddEntry={handleAddEntry} />
+        <TableComponent entries={entries} />
+      </div>
     </div>
   );
 }
