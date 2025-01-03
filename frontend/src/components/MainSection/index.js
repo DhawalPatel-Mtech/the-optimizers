0804214d37aaa2cf05aea67fd5d67be1af5efded
deepkiyada <deep.kiyada@marutitech.com>
2025-01-03 18:03:04 +0530

    categories final

--- frontend/src/components/MainSection/index.js
+++ frontend/src/components/MainSection/index.js
@@ -7,25 +7,7 @@ import { useEffect } from "react";
 import { fetchData } from "@/api/categories";
 
 export default function Main() {
-  // Function to fetch data
-  const getCategory = async (type) => {
-    console.log("Fetching data...");
-    try {
-      const response = await fetchData(type);
-      console.log("Data fetched:", response.data);
-      setCategories(response.data); // Store data in state
-      setLoading(false); // Set loading to false once data is fetched
-    } catch (error) {
-      console.error("Error fetching data:", error);
-      setError(error); // Set error state in case of failure
-      setLoading(false); // Set loading to false even if an error occurs
-    }
-  };
 
-  // Call fetchData when the component is mounted
-  useEffect(() => {
-    getCategory();
-  }, []);
 
   return (
     <div className={styles.main}>
